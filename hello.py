74b37cf7c482623700aae2f9f2b322aac9738e07
Jen Truong <jentruong@mac.lan>
2025-02-09 15:56:04 -0500

    initial push

---
+++ hello.py
@@ -0,0 +1,24 @@
+# can run this by clicking the play button in the top right corner
+
+# print() is a built-in function in Python that prints the specified message to the screen
+print("Hello, Python!")
+
+# Variables and Data Types in Python
+name = "Alice"  # String
+age = 25        # Integer
+height = 5.6    # Float
+is_student = True  # Boolean
+
+print(type(name))    # <class 'str'>
+print(type(age))     # <class 'int'>
+print(type(height))  # <class 'float'>
+print(type(is_student))  # <class 'bool'>
+
+# script to ask for user input
+user_name = input("Enter your name: ")
+print("Hello, " + user_name + "!")
+
+
+# in terminal, type python to run the python shell
+# type print("This is interactive mode!"), then press enter
+# type exit() or ctrl + d to exit the shell
